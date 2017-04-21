9ca312a6dedb18fade76e8f91e822caca7c8c5cb
CodenameLambda <dodo2998@gmail.com>
2017-04-21 19:37:06 +0200

    short_types added.

---
+++ short_types.hh
@@ -0,0 +1,26 @@
+#ifndef SDL_SHORT_TYPES_HH
+#define SDL_SHORT_TYPES_HH
+namespace SDL {
+    using cbool = const bool;
+    using cchar = const char;
+    using uchar = unsigned char;
+    using cuchar = const unsigned char;
+    using cshort = const short;
+    using ushort = unsigned short;
+    using cushort = const unsigned short;
+    using cint = const int;
+    using uint = unsigned int;
+    using cuint = const unsigned int;
+    using clong = const long;
+    using ulong = unsigned long;
+    using culong = const unsigned long;
+    using llong = long long;
+    using cllong = const long long;
+    using ullong = unsigned long long;
+    using cullong = const unsigned long long;
+    using cfloat = const float;
+    using cdouble = const double;
+    using ldouble = long double;
+    using cldouble = const long double;
+}
+#endif
