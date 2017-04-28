bfdabf519fede9b0750974af94f987d85cb69717
CodenameLambda <dodo2998@gmail.com>
2017-04-28 18:48:22 +0200

    Added to_string for version.

--- version.hh
+++ version.hh
@@ -88,6 +88,37 @@ namespace SDL {
     extern const Version<> objective_sdl_version;
     extern const Version<> sdl_linked_version;
     extern const Version<> sdl_compiled_version;
+
+    template <size_t ve>
+    std::string to_string(const VersionType vt) {
+        switch (vt) {
+            case VersionType::alpha:
+                return "alpha";
+            case VersionType::beta:
+                return "beta";
+            case VersionType::rc:
+                return "RC";
+            case VersionType::release:
+                return "release";
+        }
+    }
+
+    template <size_t ve>
+    std::string to_string(const Version<ve> v, bool include_release=false) {
+        std::string out = "";
+        bool first = true;
+        for (const auto& i : v) {
+            out += std::to_string(v);
+            if (first)
+                first = false;
+            else
+                out += '.';
+        }
+        if (!include_release && v.type == VersionType::release)
+            return out;
+        else
+            return out + " " + to_string(v.type);
+    }
 }
 
 
