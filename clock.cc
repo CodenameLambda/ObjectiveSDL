2df453fd48b8bf11aef5f94105a5446336c8d90b
CodenameLambda <dodo2998@gmail.com>
2017-04-28 19:05:37 +0200

    Fixed clock.

--- clock.cc
+++ clock.cc
@@ -2,28 +2,30 @@
 #include <thread>
 
 
-Clock::Clock() {
-    last = std::chrono::system_clock::now();
-    first = true;
-}
+namespace SDL {
+    Clock::Clock() {
+        last = std::chrono::system_clock::now();
+        first = true;
+    }
 
-std::chrono::milliseconds Clock::tick() {
-    const auto current = std::chrono::system_clock::now();
-    std::chrono::milliseconds delta = std::chrono::duration_cast<std::chrono::milliseconds>(
-            current - this->last
-    );
-    this->last = current;
-    return delta;
-}
-
-std::chrono::milliseconds Clock::tick(const float fps) {
-    const auto delta = this->tick();
-    const auto min_delta = std::chrono::duration<double>(1.0 / double(fps));
-    const auto delta_delta = delta - min_delta;
-    if (delta_delta > std::chrono::duration<double>(0.)) {
-        std::this_thread::sleep_for(delta_delta);
-        return std::chrono::duration_cast<std::chrono::milliseconds>(min_delta);
-    } else {
+    std::chrono::milliseconds Clock::tick() {
+        const auto current = std::chrono::system_clock::now();
+        std::chrono::milliseconds delta = std::chrono::duration_cast<std::chrono::milliseconds>(
+                current - this->last
+        );
+        this->last = current;
         return delta;
+    }
+
+    std::chrono::milliseconds Clock::tick(const float fps) {
+        const auto delta = this->tick();
+        const auto min_delta = std::chrono::duration<double>(1.0 / double(fps));
+        const auto delta_delta = delta - min_delta;
+        if (delta_delta > std::chrono::duration<double>(0.)) {
+            std::this_thread::sleep_for(delta_delta);
+            return std::chrono::duration_cast<std::chrono::milliseconds>(min_delta);
+        } else {
+            return delta;
+        }
     }
 }
