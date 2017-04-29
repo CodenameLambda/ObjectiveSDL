8cd596cc1986696eee4d6e258638fe126d73696e
CodenameLambda <dodo2998@gmail.com>
2017-04-29 14:08:00 +0200

    Added window_event.

---
+++ window_event.cc
@@ -0,0 +1,85 @@
+#include "window_event.hh"
+
+
+namespace SDL {
+    namespace events {
+        Event* construct_window_event(SDL_Event ev) {
+            return window_event_types.at(ev.window.type)(ev);
+        }
+
+        WindowEvent::WindowEvent()
+        : BuiltinEvent() {
+            auto& e = this->underlying_event.window;
+            e.type = SDL_WINDOWEVENT;
+            e.timestamp = 0;
+            e.windowID = 0;
+            e.event = this->window_event_id;
+            e.data1 = 0;
+            e.data2 = 0;
+        }
+
+        WindowMovedEvent::WindowMovedEvent()
+        : WindowEvent(), x(underlying_event.window.data1), y(underlying_event.window.data2) {
+        }
+
+        WindowMovedEvent::WindowMovedEvent(const SDL_Event& ev)
+        : WindowEvent(ev), x(underlying_event.window.data1), y(underlying_event.window.data2) {
+        }
+
+        WindowMovedEvent::WindowMovedEvent(const ssize_t x, const ssize_t y) : WindowMovedEvent() {
+            this->x = x;
+            this->y = y;
+        }
+
+        WindowResizedEvent::WindowResizedEvent()
+        : WindowEvent(), w(underlying_event.window.data1), h(underlying_event.window.data2) {
+        }
+
+        WindowResizedEvent::WindowResizedEvent(const SDL_Event& ev)
+        : WindowEvent(ev), w(underlying_event.window.data1), h(underlying_event.window.data2) {
+        }
+
+        WindowResizedEvent::WindowResizedEvent(const size_t w, const size_t h)
+        : WindowResizedEvent() {
+            this->w = w;
+            this->h = h;
+        }
+
+        WindowSizeChangedEvent::WindowSizeChangedEvent()
+        : WindowEvent(), w(underlying_event.window.data1), h(underlying_event.window.data2) {
+        }
+
+        WindowSizeChangedEvent::WindowSizeChangedEvent(const SDL_Event& ev)
+        : WindowEvent(ev), w(underlying_event.window.data1), h(underlying_event.window.data2) {
+        }
+
+        WindowSizeChangedEvent::WindowSizeChangedEvent(const size_t w, const size_t h)
+        : WindowSizeChangedEvent() {
+            this->w = w;
+            this->h = h;
+        }
+
+        const std::unordered_map<size_t, std::function<Event*(SDL_Event)>> window_event_types {
+            {SDL_WINDOWEVENT_SHOWN, [](SDL_Event e){ return new WindowShownEvent(e); }},
+            {SDL_WINDOWEVENT_HIDDEN, [](SDL_Event e){ return new WindowHiddenEvent(e); }},
+            {SDL_WINDOWEVENT_EXPOSED, [](SDL_Event e){ return new WindowExposedEvent(e); }},
+            {SDL_WINDOWEVENT_MOVED, [](SDL_Event e){ return new WindowMovedEvent(e); }},
+            {SDL_WINDOWEVENT_RESIZED, [](SDL_Event e){ return new WindowResizedEvent(e); }},
+            {SDL_WINDOWEVENT_SIZE_CHANGED, [](SDL_Event e){ return new WindowSizeChangedEvent(e); }},
+            {SDL_WINDOWEVENT_MINIMIZED, [](SDL_Event e){ return new WindowMinimizedEvent(e); }},
+            {SDL_WINDOWEVENT_MAXIMIZED, [](SDL_Event e){ return new WindowMaximizedEvent(e); }},
+            {SDL_WINDOWEVENT_RESTORED, [](SDL_Event e){ return new WindowRestoredEvent(e); }},
+            {SDL_WINDOWEVENT_ENTER, [](SDL_Event e){ return new WindowGainedMouseFocusEvent(e); }},
+            {SDL_WINDOWEVENT_LEAVE, [](SDL_Event e){ return new WindowLostMouseFocusEvent(e); }},
+            {SDL_WINDOWEVENT_FOCUS_GAINED, [](SDL_Event e){
+                    return new WindowGainedKeyboardFocusEvent(e);
+            }},
+            {SDL_WINDOWEVENT_FOCUS_LOST, [](SDL_Event e){
+                    return new WindowLostKeyboardFocusEvent(e);
+            }},
+            {SDL_WINDOWEVENT_CLOSE, [](SDL_Event e){ return new WindowClosedEvent(e); }},
+            {SDL_WINDOWEVENT_TAKE_FOCUS, [](SDL_Event e){ return new WindowFocusOfferedEvent(e); }},
+            {SDL_WINDOWEVENT_HIT_TEST, [](SDL_Event e){ return new WindowHitTestEvent(e); }}
+        };
+    }
+}
---
+++ window_event.hh
@@ -0,0 +1,128 @@
+#ifndef OBJSDL_WINDOW_EVENT_HH
+#define OBJSDL_WINDOW_EVENT_HH
+#include "event.hh"
+
+
+namespace SDL {
+    namespace events {
+        Event* construct_window_event(SDL_Event ev);
+
+        class WindowEvent : public BuiltinEvent {
+            public:
+            using BuiltinEvent::BuiltinEvent;
+
+            size_t window_event_id;
+
+            WindowEvent();
+        };
+
+        class WindowShownEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_SHOWN;
+        };
+
+        class WindowHiddenEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_HIDDEN;
+        };
+
+        class WindowExposedEvent : public WindowEvent {  // should be redrawn
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_EXPOSED;
+        };
+
+        class WindowMovedEvent : public WindowEvent {
+            public:
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_MOVED;
+
+            Sint32& x;
+            Sint32& y;
+
+            WindowMovedEvent();
+            WindowMovedEvent(const SDL_Event& ev);
+            WindowMovedEvent(const ssize_t x, const ssize_t y);
+        };
+
+        class WindowResizedEvent : public WindowEvent {  // not for API call
+            public:
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_RESIZED;
+
+            Sint32& w;
+            Sint32& h;
+
+            WindowResizedEvent();
+            WindowResizedEvent(const SDL_Event& ev);
+            WindowResizedEvent(const size_t w, const size_t h);
+        };
+
+        class WindowSizeChangedEvent : public WindowEvent {
+            public:
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_SIZE_CHANGED;
+
+            Sint32& w;
+            Sint32& h;
+
+            WindowSizeChangedEvent();
+            WindowSizeChangedEvent(const SDL_Event& ev);
+            WindowSizeChangedEvent(const size_t w, const size_t h);
+        };
+
+        class WindowMinimizedEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_MINIMIZED;
+        };
+
+        class WindowMaximizedEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_MAXIMIZED;
+        };
+
+        class WindowRestoredEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_RESTORED;
+        };
+
+        class WindowGainedMouseFocusEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_ENTER;
+        };
+
+        class WindowLostMouseFocusEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_LEAVE;
+        };
+
+        class WindowGainedKeyboardFocusEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_FOCUS_GAINED;
+        };
+
+        class WindowLostKeyboardFocusEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_FOCUS_LOST;
+        };
+
+        class WindowClosedEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_CLOSE;
+        };
+
+        class WindowFocusOfferedEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_TAKE_FOCUS;
+
+            void take() const;
+        };
+
+        class WindowHitTestEvent : public WindowEvent {
+            using WindowEvent::WindowEvent;
+            size_t window_event_id = SDL_WINDOWEVENT_HIT_TEST;
+        };
+
+        extern const std::unordered_map<size_t, std::function<Event*(SDL_Event)>> window_event_types;
+    }
+}
+#endif
